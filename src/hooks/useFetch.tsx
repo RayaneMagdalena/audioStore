5e42e30c572b449a176a824585da479edef93e69
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-14 20:30:21 -0300

    useFetch Hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,38 @@
+// Hooks
+import { useState, useEffect } from "react";
+// Type
+import { Product } from "../types/productType";
+
+export const useFetch = () => {
+  const [data, setData] = useState<Product[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const url = "https://run.mocky.io/v3/04a1f277-e7d7-4bca-890c-b7ed007ccce5";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+
+        if (Array.isArray(json)) {
+          setData(json);
+        } else {
+          setData([]);
+        }
+      } catch (error) {
+        console.log(error.message);
+        setError("There was some error");
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
