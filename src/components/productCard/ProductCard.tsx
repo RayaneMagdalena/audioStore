25a35f0257a21ecef9ff8025bb06505294dec284
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-14 20:15:36 -0300

    Product Card Component

---
+++ src/components/productCard/ProductCard.tsx
@@ -0,0 +1,47 @@
+// Styles
+import styles from "./ProductCard.module.css";
+// image
+import headset from "../../../public/images/headset.png";
+// Component
+import CardDetails from "../cardDetails/CardDetails";
+
+type ProductCardProps = {
+  name: string;
+  price: string;
+  reviews: number;
+  rating: number;
+  renderCardDetails:boolean;
+};
+
+const ProductCard: React.FC<ProductCardProps> = ({ 
+  name, 
+  price, 
+  rating, 
+  reviews, 
+  renderCardDetails 
+}) => {
+
+
+  // If card details is rendered add special class
+  const cardClasses = `${styles.cardItem} ${renderCardDetails ? styles['special'] : ''}`;
+
+  return (
+    <div className={cardClasses}>
+
+      <img src={headset} alt="" className={styles.image} />
+
+      <div className={styles.details}>
+        <h1 className={styles.title}>{name}</h1>
+        <p className={styles.price}>{price}</p>
+      </div>
+
+      {renderCardDetails &&  <CardDetails 
+      reviews={reviews}
+      rating={rating}
+      />}
+     
+    </div>
+  );
+};
+
+export default ProductCard;
