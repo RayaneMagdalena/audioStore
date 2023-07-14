e75350a98fb040d1246476b4c6a4686218385342
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-14 20:31:34 -0300

    Create Project Route

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// Styles
+import "./App.css";
+
+// Pages
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+import Home from "./pages/home/Home";
+import AllProducts from "./pages/allProducts/AllProducts";
+import Search from "./pages/search/Search";
+import Product from "./pages/product/Product";
+
+// Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
