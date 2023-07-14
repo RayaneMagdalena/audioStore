58054f3f4bb1110d2abafba357c232f1f3fd1b9b
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-14 20:05:22 -0300

    firebase configuration

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBSf4sXN5Ga11RbJa7gPJ-zUwWzqDszU8M",
+  authDomain: "audio-6294d.firebaseapp.com",
+  projectId: "audio-6294d",
+  storageBucket: "audio-6294d.appspot.com",
+  messagingSenderId: "856451492707",
+  appId: "1:856451492707:web:2454dda39837d6942b0156"
+})
+
+export const auth = getAuth(firebaseApp)
+export default firebaseApp
