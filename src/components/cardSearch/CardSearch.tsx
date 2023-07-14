2c6b80d067fad559d86c8d7f21fc1c9ea70b6751
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-14 20:23:36 -0300

    Card Search Component

---
+++ src/components/cardSearch/CardSearch.tsx
@@ -0,0 +1,60 @@
+// Styles
+import styles from "./CardSearch.module.css";
+// Image and Icon
+import headset from "../../../public/images/headset.png";
+import star from "../../../public/images/star-filled.svg";
+import more from "../../../public/images/icon-more-vertical.svg";
+
+interface ProductCardProps {
+  name: string;
+  price: string;
+  rating: number;
+  reviews: number;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
+  name,
+  price,
+  rating,
+  reviews,
+}) => {
+
+  return (
+    <div className={styles.cardItem}>
+      <div className={styles.imageContainer}>
+        <img 
+        src={headset} 
+        alt="" 
+        className={styles.cardImage} 
+        />
+      </div>
+
+      <div className={styles.cardInfo}>
+        
+         <h1 className={styles.cardTitle}>{name}</h1>
+         <p className={styles.cardPrice}>{price}</p>
+
+         <div className={styles.cardDetails}>
+           <div className={styles.detailsContainer}>
+             <img 
+             src={star} 
+             alt="" 
+             className={styles.iconStar} 
+             />
+             <p className={styles.detail}>{rating}</p>
+             <p className={styles.detail}>{reviews} Reviews</p>
+           </div>
+
+           <img 
+           src={more} 
+           alt="" 
+           className={styles.iconMore} 
+           />
+        </div>
+     
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
