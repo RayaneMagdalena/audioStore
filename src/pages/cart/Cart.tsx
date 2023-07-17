ca2c6d96bff7b28b1afce8648f681baf19cd483e
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-17 18:55:05 -0300

    Cart page

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,52 @@
+
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
+import styles from "./Cart.module.css";
+import headset from "../../../public/images/headset.png";
+import plus from "../../../public/images/icon-plus.svg";
+import minus from "../../../public/images/icon-minus.svg";
+import trash from "../../../public/images/icon-trash.svg";
+import chevron from "../../../public/images/button-icon-chevron-right.svg";
+
+const Cart = () => {
+  const { cartProducts } = useContext(CartContext);
+
+  return (
+    <div className={styles.cartContainer}>
+      <div className={styles.cardCart}>
+        <img src={headset} alt="" className={styles.imageCart} />
+
+        {cartProducts.map((product) => (
+          <div key={product.id}>
+            <h2>{product.price}</h2>
+            <h1 className={styles.title}>{product.name}</h1>
+            <p className={styles.price}>{product.price}</p>
+            <div className={styles.cardFunctions}>
+              <div className={styles.quantityFunction}>
+                <img src={minus} alt="" className={styles.iconMinus} />
+                <p className={styles.quantifyItem}>{product.quantity}</p>
+                <img src={plus} alt="" className={styles.iconPlus} />
+              </div>
+              <img src={trash} alt="" className={styles.iconDelete} />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <div className={styles.totalPurchase}>
+          <p className={styles.totalItems}>Total {cartProducts.length} items</p>
+          <p className={styles.totalPrice}>USD 295</p>
+        </div>
+
+        <button className={styles.checkoutButton}>
+          <p className={styles.titleButton}>Proceed to Checkout</p>
+          <img src={chevron} alt="" className={styles.iconChevron} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
+
