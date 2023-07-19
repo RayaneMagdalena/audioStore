492d1ae51c8f3cd4d878e71bb8280ae6b816927f
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2023-07-19 12:38:56 -0300

    Reviews Component

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,34 +1,51 @@
-import styles from './Product.module.css';
+// Styles
+import styles from "./Product.module.css";
+// images and icons
+import headset_2 from "../../../public/images/headset_2.svg";
+// hooks
 import { useParams, Link } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
-import headset_2 from '../../../public/images/headset_2.svg'
-import starReview from '../../../public/images/starReview.svg'
-import user from '../../../public/images/user.svg';
+// Components
+import Reviews from "../../components/reviews/Reviews";
 import Carousel from "../../components/carousel/Carousel";
 // context
 import { useContext } from "react";
-import { CartContext } from '../../contexts/CartContext'; 
-import { useNavigate } from 'react-router-dom';
+import { CartContext } from "../../contexts/CartContext";
+import { useNavigate } from "react-router-dom";
+
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+  reviews: Review[];
+}
+
+interface Review {
+  id: number;
+  user: string;
+  rating: number;
+  description: string;
+}
 
 const Product = () => {
   const { addToCart } = useContext(CartContext);
   const navigate = useNavigate();
 
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
 
   const { data: products } = useFetch();
 
-//  Add product to cart
-const handleAddToCart = (product) => {
-  addToCart(product);
-  navigate("/cart")
-};
-
+  //  Add product to cart
+  const handleAddToCart = (product: Product) => {
+    addToCart(product);
+    navigate("/cart");
+  };
 
   if (!products) {
     return <div>Loading...</div>;
   }
 
+  // Find product by id
   const product = products.find((product) => product.id === parseInt(id));
 
   if (!product) {
@@ -45,48 +62,31 @@ const handleAddToCart = (product) => {
         <button className={styles.buttonInfo}>Features</button>
       </div>
 
-<div className={styles.productImageContainer}>
-      <img 
-      src={headset_2} 
-      alt=""
-      className={styles.productImage}
-      />
+      <div className={styles.productImageContainer}>
+        <img src={headset_2} alt="" className={styles.productImage} />
       </div>
 
       <p className={styles.subtitle}>Reviews ({product.reviews.length})</p>
 
-      {product.reviews.map((review) => (
-        <div key={review.id} className={styles.productReview}>
-          <img 
-          src={user} 
-          alt=""
-          className={styles.reviewImageUser}
-          />
+      <Reviews reviews={product.reviews} />
 
-          <div>
-          <h3 className={styles.reviewUser}>{review.user}</h3>
-          <img 
-          src={starReview} 
-          alt=""
-          className={styles.starReview}
-          />
-          <p className={styles.reviewDescription}>{review.description}</p>
-          </div>
-        </div>
-      ))}
+      <p className={styles.subtitle}>
+        Another Product{" "}
+        <Link to="/products" className={styles.seeAll}>
+          {" "}
+          See All
+        </Link>
+      </p>
 
-      <p className={styles.subtitle}>Another Product  <Link to='/products' className={styles.seeAll}> See All</Link></p>
+      <Carousel products={products} />
 
-      <Carousel 
-      products={products}
-      />
-
-      <button 
-      onClick={() => handleAddToCart(product)}
-      className={styles.buttonAdd}>Add To Cart</button>
+      <button
+        onClick={() => handleAddToCart(product)}
+        className={styles.buttonAdd}
+      >
+        Add To Cart
+      </button>
     </div>
-
-
   );
 };
 
